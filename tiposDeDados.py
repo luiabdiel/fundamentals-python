941d8f7bc951f573c80029397cfc6312d7d7abdf
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-20 11:20:29 -0300

    tipos de dados

---
+++ tiposDeDados.py
@@ -0,0 +1,9 @@
+inteiro: int = 20
+ponto_flutuante: float = 20.5
+verdadeiro_falso: bool = True | False
+cadeia_de_caracteres: str = 'Hi, python'
+
+print(type(10))
+print(type(10.5))
+print(type(True))
+print(type("string"))
