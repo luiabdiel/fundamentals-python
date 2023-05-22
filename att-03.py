d90d358cff2dc417fa2f5e88c1728032768c6d8c
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-22 16:22:37 -0300

    att-03: approval message

--- att-03.py
+++ att-03.py
@@ -10,4 +10,9 @@ def academic_average(grade1, grade2, grade3, grade4):
 
 result = academic_average(gradeOne, gradeTwo, gradeThree, gradeFour)
 
+if result >= 7:
+    print('Congratulations, you have passed!!!')
+else:
+    print('you have failed :(')
+
 print('Your school grade is: {}'.format(result))
