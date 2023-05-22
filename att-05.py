68eae80946357955d4bef2349a81be529117d221
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-22 16:50:48 -0300

    att-05

---
+++ att-05.py
@@ -0,0 +1,9 @@
+numberOne = int(input('Enter a number:  '))
+numberTwo = int(input('Enter another number: '))
+
+if numberOne > numberTwo:
+    print(numberOne)
+elif numberOne < numberTwo:
+    print(numberTwo)
+else:
+    print('The numbers are equal')
